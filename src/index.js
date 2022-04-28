e93e930ee68394da60e58a921361a8e2d3659b92
dcatano <dcatanor@unal.edu.co>
2022-04-28 15:00:20 -0500

    Client connection

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import app from './app';
 import { Server as WebSocketServer } from 'socket.io';
 import http from 'http';
 import {connectDB} from "./db";
+import sockets from "./sockets";
 
 connectDB()
 const server = http.createServer(app);
 const httpServer = server.listen(3000)
 const io = new WebSocketServer(httpServer);
+sockets(io);
 
